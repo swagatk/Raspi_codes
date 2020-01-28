49f60c93b42ac65db5394fd5574a41e9fd4d2fae
swagat.kumar <swagat.kumar@gmail.com>
2020-01-28 16:40:48 +0000

    Traffic Light simulator code is completed and tested.

--- GPIO_CODES/WS1/traffic_light_simulator.py
+++ GPIO_CODES/WS1/traffic_light_simulator.py
@@ -22,8 +22,8 @@ Buzzer = 22
 
 
 # LEDs for Pedestrians
-Ped_Red = 18
-Ped_Green = 24
+Ped_Red = 10
+Ped_Green = 11
 
 
 
@@ -53,9 +53,11 @@ GPIO.output(Ped_Green, GPIO.LOW)
 def startgreen():
     # Remember all code in the function is indented
     GPIO.output(Car_Green, GPIO.HIGH)
+    GPIO.output(Car_Amber, GPIO.LOW)
+    GPIO.output(Car_Red, GPIO.LOW)
+    
     GPIO.output(Ped_Green, GPIO.LOW)
     GPIO.output(Ped_Red, GPIO.HIGH)
-    GPIO.output(Car_Amber, GPIO.LOW)
     
  
 # Turn the green off and the amber on for 3 seconds
@@ -63,8 +65,12 @@ def startgreen():
 def steadyamber():
     # Remember all code in the function is indented
     GPIO.output(Car_Green, GPIO.LOW)
+    GPIO.output(Car_Red, GPIO.LOW)
     GPIO.output(Car_Amber, GPIO.HIGH)
+    
     GPIO.output(Ped_Red, GPIO.HIGH)
+    GPIO.output(Ped_Green, GPIO.LOW)
+    
     time.sleep(3)
  
 # Turn the amber off, and then the red on for 1 second
@@ -78,8 +84,9 @@ def steadyred():
 # Sound the buzzer for 4 seconds
 # (If you have the 'pedestrian' LEDs, turn the red off and green on)
 def startwalking():
- # Make the buzzer buzz on and off, half a second of
- # sound followed by half a second of silence
+# Make the buzzer buzz on and off, half a second of
+# sound followed by half a second of silence
+
     GPIO.output(Ped_Red, GPIO.LOW)
     GPIO.output(Ped_Green, GPIO.HIGH)
     for _ in range(4):
@@ -92,6 +99,10 @@ def startwalking():
 # off for the two seconds)
 def dontwalk():
     # Remember all code in the function is indented
+    GPIO.output(Car_Red, GPIO.HIGH)
+    GPIO.output(Car_Amber, GPIO.LOW)
+    GPIO.output(Ped_Red, GPIO.LOW)
+    
     GPIO.output(Buzzer, GPIO.LOW)
     for _ in range(2):
         GPIO.output(Ped_Green, GPIO.LOW)
@@ -103,6 +114,9 @@ def dontwalk():
 # (And the green 'pedestrian' LED too)
 def flashingambergreen():
     # Remember all code in the function is indented
+    GPIO.output(Car_Red, GPIO.HIGH)
+    GPIO.output(Ped_Red, GPIO.LOW)
+    
     for _ in range(6):
         GPIO.output(Car_Amber, GPIO.HIGH)
         GPIO.output(Ped_Green, GPIO.HIGH)
@@ -115,25 +129,43 @@ def flashingambergreen():
 # (Turn the green 'pedestrian' LED off and the red on)
 def flashingamber():
     # Remember all code in the function is indented
+    GPIO.output(Ped_Green, GPIO.LOW)
+    GPIO.output(Ped_Red, GPIO.HIGH)
+    
     GPIO.output(Car_Amber, GPIO.LOW)
     time.sleep(0.5)
     GPIO.output(Car_Amber, GPIO.HIGH)
     time.sleep(0.5)
+
+def button_is_pressed():
+    
+    if GPIO.input(Button) == False:
+        print("Button is Pressed. Wait for Signal Change")
+        return True
+    else:
+        return False
     
  
 # Go through the traffic light sequence by calling each function
 # one after the other.
 def trafficlightqequence():
     # Remember all code in the function is indented
-    steadygreen()
     steadyamber()
     steadyred()
+    startwalking()
+    dontwalk()
+    flashingambergreen()
+    flashingamber()
+    startgreen()
 
 #############
  
 os.system('clear') # Clears the terminal
+
 print("Traffic Lights")
 # Initialise the traffic lights
+print('Original Button State:', GPIO.input(Button))
+
 startgreen()
 # Here is the loop that waits at least 20 seconds before
 # stopping the cars if the button has been pressed
@@ -142,9 +174,10 @@ while True: # Loop around forever
     start = time.time() # Records the current time
     while buttonnotpressed: # While the button has not been pressed
         time.sleep(0.1) # Wait for 0.1s
-        if button.ispressed: # If the button is pressed
+        if button_is_pressed(): # If the button is pressed
             now = time.time()
             buttonnotpressed = False # Button has been pressed
             if (now - start) <= 20: # If under 20 seconds
                 time.sleep(20 - (now - start)) # Wait until 20s is up
                 trafficlightqequence() # Run the traffic light sequence
+GPIO.cleanup()
