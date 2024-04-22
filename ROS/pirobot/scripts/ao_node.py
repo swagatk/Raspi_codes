974439b7adf00f9de5edc537df041a8c16877aa5
Swagat Kumar <swagat.kumar@gmail.com>
2024-04-22 09:45:48 +0100

    added avoid obstacle node to ROS

---
+++ ROS/pirobot/scripts/ao_node.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+from avoid_obstacle import ao_main, initialize_sensor
+def move():
+    pub = rospy.Publisher('status', String, queue_size=10)
+    rospy.init_node('avoid_obstacle', anonymous=True)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        msg = ao_main()
+        rospy.loginfo(msg)
+        pub.publish(msg)
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        initialize_sensor()
+        move()
+    except rospy.ROSInterruptException:
+        rospy.loginfo('Keyboard Interrupt')
